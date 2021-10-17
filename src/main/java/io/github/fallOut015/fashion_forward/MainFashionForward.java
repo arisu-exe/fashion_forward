76a9d3d05005501319cd95a4645b4f42bc598da9
fallOut015 <captainDoodles264@gmail.com>
2021-10-17 00:17:26 -0700

    basic stuff

---
+++ src/main/java/io/github/fallOut015/fashion_forward/MainFashionForward.java
@@ -0,0 +1,41 @@
+package io.github.fallOut015.fashion_forward;
+
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.InterModEnqueueEvent;
+import net.minecraftforge.fml.event.lifecycle.InterModProcessEvent;
+import net.minecraftforge.fmlserverevents.FMLServerStartingEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+@Mod(MainFashionForward.MODID)
+public class MainFashionForward {
+    public static final String MODID = "fashion_forward";
+    private static final Logger LOGGER = LogManager.getLogger();
+
+    public MainFashionForward() {
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::enqueueIMC);
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::processIMC);
+
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    private void setup(final FMLCommonSetupEvent event) {
+    }
+    private void enqueueIMC(final InterModEnqueueEvent event) {
+    }
+    private void processIMC(final InterModProcessEvent event) {
+    }
+
+    @SubscribeEvent
+    public void onServerStarting(FMLServerStartingEvent event) {
+    }
+
+    @Mod.EventBusSubscriber(bus=Mod.EventBusSubscriber.Bus.MOD)
+    public static class RegistryEvents {
+    }
+}
